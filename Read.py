09cd6dabb6a066f764d95f556bb63d83d0ad2417
pulipati2021 <94949196+pulipati2021@users.noreply.github.com>
2023-03-20 09:35:12 +0530

    Added working code
    Added working code

---
+++ Read.py
@@ -0,0 +1,36 @@
+
+from googleapiclient.discovery import build
+
+from google.oauth2 import service_account
+
+
+SERVICE_ACCOUNT_FILE = 'sales_keys.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# If modifying these scopes, delete the file token.json.
+
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1gaMRyTDBAe0jIl0GGZDClaxIklqLMTmTcSV1f6x1mdA'
+ 
+
+
+service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+sheet = service.spreadsheets()
+#result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,range="Sales!A1:G2").execute()
+#rowcount = sheet.values().get(SAMPLE_SPREADSHEET_ID, range="Sales!A1:G2").execute().getValues().size()
+#values = result.get('values', [])
+range = 'Sales!C:C'
+
+rows = service.spreadsheets().values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range).execute().get('values', [])
+last_row = rows[-1] if rows else None
+last_row_id = len(rows)
+#print(last_row_id, last_row)
+print(last_row)
+ 
+ 
