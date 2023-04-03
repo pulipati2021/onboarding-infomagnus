f085f081c597023128d89330499b41f7fb9d7942
pulipati2021 <94949196+pulipati2021@users.noreply.github.com>
2023-04-03 14:24:57 +0530

    Update verify_read.py

--- verify_read.py
+++ verify_read.py
@@ -39,9 +39,9 @@ for i,row in enumerate(rows):
          # complete_emails.append(email)
           email_data[email]= row
 #-------------------------------- 
-
-file=open("/home/runner/work/onboarding-infomagnus/onboarding-infomagnus/completed.txt", "r")  
-fileRead = open("/home/runner/work/onboarding-infomagnus/onboarding-infomagnus/completed.txt","a")  
+#/home/runner/work/onboarding-infomagnus/onboarding-infomagnus/
+file=open("completed.txt", "r")  
+fileRead = open("completed.txt","a")  
 content = file.read()    
 for emailkey,emailvalue in enumerate(email_data):
    #print(emailvalue)
